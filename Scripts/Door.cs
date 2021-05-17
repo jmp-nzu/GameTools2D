d2f6df29ef17690b9819b5b23f354d85c401cf15
Jean-Marc Pelletier <jmp@jmpelletier.com>
2021-05-17 20:39:27 +0900

    ドアの切り替えを実装

--- Scripts/Door.cs
+++ Scripts/Door.cs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+ 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,5 +24,10 @@ public class Door : MonoBehaviour
     public void Close()
     {
         animator.SetBool("open", false);
+    }
+
+    public void Toggle()
+    {
+        animator.SetBool("open", !animator.GetBool("open"));
     }
 }
